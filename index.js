2faa14c96b7dcceed64c5882190b0003d122c350
PabloSczn <sancheznarro.pablo@gmail.com>
2024-01-13 17:10:12 +0000

    Improved format

--- index.js
+++ index.js
@@ -1,64 +1,76 @@
+// Importing necessary modules
 const express = require('express');
 const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 
+// Importing the Product model
 const Product = require('./models/product')
 
+// Connecting to MongoDB
 mongoose.connect('mongodb://127.0.0.1:27017/farmStand')
     .then(() => {
         console.log("Mongo Open!")
     })
     .catch(err => {
-        console.log("Error occured in Mongo!")
+        console.log("Error occurred in Mongo!")
         console.log(err)
     })
 
+// Setting up the view engine and middleware
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
+// Handling GET request for listing all products
 app.get('/products', async (req, res) => {
-    const products = await Product.find({})
-    res.render('products/index', {products});
+    const products = await Product.find({});
+    res.render('products/index', { products });
 })
 
+// Handling GET request for creating a new product
 app.get('/products/new', (req, res) => {
-    res.render('products/new')
+    res.render('products/new');
 })
 
-app.post('/products', async(req, res) => {
+// Handling POST request for creating a new product
+app.post('/products', async (req, res) => {
     const newProduct = new Product(req.body);
     await newProduct.save();
     res.redirect(`/products/${newProduct._id}`);
 })
 
+// Handling GET request for editing a specific product
 app.get('/products/:id/edit', async (req, res) => {
     const id = req.params.id;
     const product = await Product.findById(id);
-    res.render('products/edit', {product})
+    res.render('products/edit', { product });
 })
 
-app.put('/products/:id', async(req, res) => {
+// Handling PUT request for updating a specific product
+app.put('/products/:id', async (req, res) => {
     const id = req.params.id;
-    const product = await Product.findByIdAndUpdate(id, req.body, {runValidators: true, new: true});
+    const product = await Product.findByIdAndUpdate(id, req.body, { runValidators: true, new: true });
     res.redirect(`/products/${product._id}`);
 })
 
+// Handling GET request for viewing details of a specific product
 app.get('/products/:id', async (req, res) => {
     const id = req.params.id;
     const product = await Product.findById(id);
-    res.render('products/details', {product})
+    res.render('products/details', { product });
 })
 
-app.delete('/products/:id', async(req, res) => {
+// Handling DELETE request for deleting a specific product
+app.delete('/products/:id', async (req, res) => {
     const id = req.params.id;
     const product = await Product.findByIdAndDelete(id);
     res.redirect('/products');
 })
 
+// Starting the server on port 3000
 app.listen(3000, () => {
-    console.log("App listening in 3000")
+    console.log("App listening on port 3000")
 })
